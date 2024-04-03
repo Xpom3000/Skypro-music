a33cc6e2c398e7ebb37656d20a9c999328d4aa29
Igor Shipitko <garikrhip20@gmail.com>
2024-04-03 20:20:09 +0300

    hw1 начало

---
+++ skypro-music/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["cyrillic"] });
+
+export const metadata: Metadata = {
+  title: "Лучший сервис в мире",
+  description: "Скайпро лучший учебный центр",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
