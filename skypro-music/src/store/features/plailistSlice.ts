4c88824ccc04ff6c6ea662c31ce7dd48f39b8abf
Igor Shipitko <garikrhip20@gmail.com>
2024-05-01 19:42:40 +0300

    hw4.1 setCurrentTrack установили рандомное shuffledPlaylist состояние для плейлиста

--- skypro-music/src/store/features/plailistSlice.ts
+++ skypro-music/src/store/features/plailistSlice.ts
@@ -2,22 +2,30 @@ import { trackType } from "@/type";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type PlaylistStateType = {
-  currentTrack: null | trackType,
-  playlist: trackType[]
+  currentTrack: null | trackType;
+  playlist: trackType[];
+  shuffledPlaylist: trackType[];
 };
 
 const initialState: PlaylistStateType = {
   currentTrack: null,
   playlist: [],
+  shuffledPlaylist: [],
 };
 
 const playlistSlice = createSlice({
   name: "playlist",
   initialState,
   reducers: {
-    setCurrentTrack: (state, action: PayloadAction<{track:trackType , tracksData: trackType[]}>) => {
+    setCurrentTrack: (
+      state,
+      action: PayloadAction<{ track: trackType; tracksData: trackType[] }>
+    ) => {
       state.currentTrack = action.payload.track;
       state.playlist = action.payload.tracksData;
+      state.shuffledPlaylist = [...action.payload.tracksData].sort(
+        () => 0.5 - Math.random()
+      );
     },
   },
 });
