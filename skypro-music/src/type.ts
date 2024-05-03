794eae8168cb0249bad578b571113469b905e98e
Igor Shipitko <garikrhip20@gmail.com>
2024-05-03 23:09:25 +0300

    3

--- skypro-music/src/type.ts
+++ skypro-music/src/type.ts
@@ -1,34 +1,32 @@
 type userType = {
-  id: number,
-  username: string,
-  first_name: string,
-  last_name: string,
-  email: string
-}
+  id: number;
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+};
 
 export type FilterItemType = {
   title: string;
   list: string[];
   handleFilterClick: (newFilter: string) => void;
-  isOpened: boolean;  
+  isOpened: boolean;
 };
 
 export type trackType = {
-  id: number,
-  name: string,
-  author: string,
-  release_date:string,
-  genre: string,
-  duration_in_seconds: number,
-  album: string,
-  logo: string | null,
-  track_file: string,
-  stared_user: userType[]
-  onClick: (newFilter: string) => void;
+  id: number;
+  name: string;
+  author: string;
+  release_date: string;
+  genre: string;
+  duration_in_seconds: number;
+  album: string;
+  logo: string | null;
+  track_file: string;
+  stared_user: userType[];
 };
 
 export type ErrorType = {
   error: Error;
   reset: () => void;
-
-}
+};
